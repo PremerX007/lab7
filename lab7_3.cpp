5401cd29ccfc3c64462304c79cd57d3bc0f0005b
PremerX007 <bannawat_v@cmu.ac.th>
2022-12-13 16:32:32 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,28 @@
 
 using namespace std;
 
+int adiff(int a, int b){
+  int x;
+  if (a>b){
+    x = a-b;
+  }else{
+    x = b-a;
+  }
+
+  if((x%360)>180){
+    return 360-x%360;
+  }else{
+    return x%360;
+  }
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << endl;
+  cout << adiff(210,45) << endl;
+  cout << adiff(0,360) << endl;
+  cout << adiff(10,350) << endl;//
+  cout << adiff(95,260) << endl;
+  cout << adiff(90,-90) << endl;
+  cout << adiff(1000,280) << endl;
+  cout << adiff(60,244) << endl;//
 }
